47667c8cd6c059e90f9993f4170ec2d43102cc58
Mahesh <m.konduri@outlook.com>
2019-12-18 18:47:21 -0500

    initial commit

---
+++ src/com/javaconcepts/application/MainApplication.java
@@ -0,0 +1,8 @@
+package com.javaconcepts.application;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("main projects start from here all methods calling from this main class :");
+    }
+}
